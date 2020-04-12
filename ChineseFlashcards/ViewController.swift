68681f9a56609092258e385d3520f6f1813da38d
Eric Wang <ezwang1@gmail.com>
2020-04-12 15:14:34 -0400

    Initial Commit

---
+++ ChineseFlashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChineseFlashcards
+//
+//  Created by Eric Wang on 4/12/20.
+//  Copyright © 2020 Eric Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
