f3144a1101ae4e603f07be3cddac10369c283ae5
Eric Wang <ezwang1@gmail.com>
2020-04-21 13:05:07 -0400

    add loading icons for register/login

--- ChineseFlashcards/LoginViewController.swift
+++ ChineseFlashcards/LoginViewController.swift
@@ -12,6 +12,7 @@ import FirebaseAuth
 class LoginViewController: UIViewController {
     @IBOutlet weak var fieldEmail: UITextField!
     @IBOutlet weak var fieldPassword: UITextField!
+    @IBOutlet weak var indicatorLogin: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,20 +20,25 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginClicked(button: UIButton) {
+        indicatorLogin.startAnimating()
         Auth.auth().signIn(withEmail: fieldEmail.text ?? "", password: fieldPassword.text ?? "") { [weak self] authResult, error in
             guard let strongSelf = self else { return }
             
-            if let e = error {
-                let errorMessage = getErrorMessage(code: e._code)
+            DispatchQueue.main.async {
+                if let e = error {
+                    let errorMessage = getErrorMessage(code: e._code)
+                    
+                    let alert = UIAlertController(title: "Login Error", message: errorMessage, preferredStyle: UIAlertController.Style.alert)
+                    
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                    
+                    strongSelf.present(alert, animated: true, completion: nil)
+                }
+                else if authResult != nil {
+                    strongSelf.performSegue(withIdentifier: "deck", sender: nil)
+                }
                 
-                let alert = UIAlertController(title: "Login Error", message: errorMessage, preferredStyle: UIAlertController.Style.alert)
-                
-                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                
-                strongSelf.present(alert, animated: true, completion: nil)
-            }
-            else if authResult != nil {
-                strongSelf.performSegue(withIdentifier: "deck", sender: nil)
+                strongSelf.indicatorLogin.stopAnimating()
             }
         }
     }
--- ChineseFlashcards/RegisterViewController.swift
+++ ChineseFlashcards/RegisterViewController.swift
@@ -12,24 +12,31 @@ import FirebaseAuth
 class RegisterViewController: UIViewController {
     @IBOutlet weak var fieldEmail: UITextField!
     @IBOutlet weak var fieldPassword: UITextField!
+    @IBOutlet weak var indicatorRegister: UIActivityIndicatorView!
     
     
     @IBAction func registerClicked(button: UIButton)
     {
+        self.indicatorRegister.startAnimating()
+        
         Auth.auth().createUser(withEmail: fieldEmail.text ?? "", password: fieldEmail.text ?? "") { [weak self] authResult, error in
             guard let strongSelf = self else { return }
             
-            if let e = error {
-                let errorMessage = getErrorMessage(code: e._code)
+            DispatchQueue.main.async {
+                if let e = error {
+                    let errorMessage = getErrorMessage(code: e._code)
+                    
+                    let alert = UIAlertController(title: "Registration Error", message: errorMessage, preferredStyle: UIAlertController.Style.alert)
+                    
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                    
+                    strongSelf.present(alert, animated: true, completion: nil)
+                }
+                else if authResult != nil {
+                    strongSelf.performSegue(withIdentifier: "deck", sender: nil)
+                }
                 
-                let alert = UIAlertController(title: "Registration Error", message: errorMessage, preferredStyle: UIAlertController.Style.alert)
-                
-                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                
-                strongSelf.present(alert, animated: true, completion: nil)
-            }
-            else if authResult != nil {
-                strongSelf.performSegue(withIdentifier: "deck", sender: nil)
+                strongSelf.indicatorRegister.stopAnimating()
             }
         }
     }
